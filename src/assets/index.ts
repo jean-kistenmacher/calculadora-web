94607e96a2ca7aedb729d281d4dcfb6e1095cd1a
Jean Kistenmacher <jean.kistenmacher@b2wdigital.com>
2023-11-08 21:25:41 -0300

    feat: starting project

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+const assets = {
+  images: {
+    logo: require("./images/unisc.png")
+  }
+};
+
+export default assets;
---
+++ src/configs/colorConfigs.ts
@@ -0,0 +1,17 @@
+import { colors } from "@mui/material";
+
+const colorConfigs = {
+  sidebar: {
+    bg: "#fff",
+    color: "#2168A6",
+    hoverBg: "#b6cee3",
+    activeBg: "#b6cee3"
+  },
+  topbar: {
+    bg: "#fff",
+    color: "#000"
+  },
+  mainBg: colors.grey["50"]
+};
+
+export default colorConfigs;
